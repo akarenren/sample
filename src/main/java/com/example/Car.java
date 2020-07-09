2f9730bb032769908a24a05049debad258c2c5bf
kanta-fujiwara <kanta.fujiwara@rakus.co.jp>
2020-07-09 11:37:23 +0900

    コメント/Getter/Setterを追加

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -1,6 +1,14 @@
 package com.example;
 
 public class Car {
+	/** 速度 */
+	private int speed = 1000;
 
-	private int speed;
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
 }
